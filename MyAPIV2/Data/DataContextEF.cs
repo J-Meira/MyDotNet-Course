4531c8044e631bf3a09274b31cbf4f0473b7f53e
J-Meira <jonathanmeira@hotmail.com>
2023-12-27 12:04:23 -0300

    api v2 start

---
+++ MyAPIV2/Data/DataContextEF.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MyAPIV2.Models;
+
+namespace MyAPIV2.Data
+{
+  public class DataContextEF : DbContext
+  {
+    private readonly IConfiguration _config;
+
+    public DataContextEF(IConfiguration config){
+      _config = config;
+    }
+
+    public virtual DbSet<User> Users {get; set;}
+    public virtual DbSet<UserSalary> UserSalary {get; set;}
+    public virtual DbSet<UserJobInfo> UserJobInfo {get; set;}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+                optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+      }
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.HasDefaultSchema("TutorialAppSchema");
+
+      modelBuilder.Entity<User>()
+        .ToTable("Users", "TutorialAppSchema")
+        .HasKey(c => c.UserId);
+
+      modelBuilder.Entity<UserSalary>()
+        .HasKey(c => c.UserId);
+
+      modelBuilder.Entity<UserJobInfo>()
+        .HasKey(c => c.UserId);
+    }
+    
+  }
+}
